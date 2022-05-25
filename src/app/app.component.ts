7cc24cea0184b20348d204a74aef12972227305f
PedroVilela1 <100644695+PedroVilela1@users.noreply.github.com>
2022-05-25 01:04:02 -0300

    Primeiro

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+
+})
+export class AppComponent {
+  constructor() {}
+}
